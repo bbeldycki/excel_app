e32633b9923dba7b9e657c68aba8021669198911
bbeldycki <beldyckibartosz@gmail.com>
2020-12-10 11:13:13 +0100

    Second commit in day 10.12.2020, I added opening side with template.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/hello')
+    @app.route('/')
     def hello():
-        return "hello"
+        return render_template("initial/welcome.html")
 
     return app
