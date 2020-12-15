1344c41cbae2aabc28509d0a84b442dd2c880c1a
bbeldycki <beldyckibartosz@gmail.com>
2020-12-15 14:23:28 +0100

    First commit in day 15.12.2020, I reorganize some code. I added some conditions for uploading files. Now it is possible to download final file.

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,38 @@
 from flask import Flask, render_template
 import os
 
+path = "app/static/filesuploaded"
+path1 = "app/static/col_rm_final"
+
 
 def create_app():
     app = Flask(__name__)
-    path = "app/static/filesuploaded"
-    path1 = "app/static/col_rm_final"
-    try:
-        os.makedirs(path)
-        os.makedirs(path1)
-    except OSError:
-        pass
+
+    check_catalogs_exist(path)
+    check_catalogs_exist(path1)
+
     app.config['SECRET_KEY'] = 'argagathtrhwtrh'
 
     @app.route('/')
     def hello():
+        check_catalogs_empty(path)
+        check_catalogs_empty(path1)
         return render_template("initial/welcome.html")
 
     from . import col_rm
     app.register_blueprint(col_rm.bp)
 
     return app
+
+
+def check_catalogs_exist(check_path):
+    try:
+        os.makedirs(check_path)
+    except OSError:
+        pass
+
+
+def check_catalogs_empty(check_path):
+    dirs = os.listdir(check_path)
+    for file in dirs:
+        os.remove(check_path + '/' + file)
--- app/col_rm.py
+++ app/col_rm.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, flash, redirect, render_template
+from flask import Blueprint, request, flash, redirect, render_template, send_from_directory, abort, send_file
 from werkzeug.utils import secure_filename
 from pathlib import Path
 import openpyxl as oxl
@@ -8,6 +8,9 @@ bp = Blueprint('colclear', __name__)
 
 allowed_extensions = set(['txt', 'xlsx'])
 
+path = "app/static/filesuploaded"
+path1 = "app/static/col_rm_final"
+
 
 def allowed_files(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
@@ -15,13 +18,19 @@ def allowed_files(filename):
 
 @bp.route('/clupload', methods=["GET", "POST"])
 def clupload():
+    # Check if there is any "final" file in directory for final files
+    # If there is, remove it
+    dirs = os.listdir(path1)
+    for file in dirs:
+        os.remove(path1 + '/' + file)
+
     if request.method == "POST":
 
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
 
-        if 'file' not in request.files:
+        if 'file1' not in request.files:
             flash('No file part')
             return redirect(request.url)
 
@@ -29,7 +38,6 @@ def clupload():
         filse1 = request.files['file1']
 
         files = [filse, filse1]
-        print(files)
         for file in files:
             if file.filename == '':
                 flash('No file selected')
@@ -39,15 +47,60 @@ def clupload():
             if file.filename.rsplit('.', 1)[1].lower() not in allowed_extensions:
                 files.remove(file)
 
+        if len(files) == 1:
+            fext = files[0].filename.split('.', 1)[1]
+            if fext == 'txt':
+                return render_template("col_rm/upload.html", numfiles=len(files), fext=True, wrong=True)
+            else:
+                return render_template("col_rm/upload.html", numfiles=len(files), fext=False, wrong=True)
+        elif len(files) == 2:
+            fext = files[0].filename.split('.', 1)[1]
+            fext1 = files[1].filename.split('.', 1)[1]
+            if fext == fext1 and fext == 'txt':
+                return render_template("col_rm/upload.html", fext=True, wrong=True)
+            elif fext == fext1 and fext == 'xlsx':
+                return render_template("col_rm/upload.html", fext=False, wrong=True)
+
         filenames = []
         for file in files:
             if file and allowed_files(file.filename):
                 filename = secure_filename(file.filename)
                 filenames.append(filename)
-                file.save(os.path.join(Path("app/static/filesuploaded"), filename))
+                file.save(os.path.join(Path(path), filename))
 
-        return render_template("col_rm/upload.html", filse=filenames)
+        return render_template("col_rm/upload.html", filse=filenames, lfse=len(filenames), wrong=False)
     return render_template("col_rm/upload.html")
+
+
+@bp.route('/removed')
+def removed():
+    dirs = os.listdir(path)
+    # fileX = ['name', 'extension']
+    file0 = [dirs[0].split('.', 1)[0], dirs[0].split('.', 1)[1]]
+    file1 = [dirs[1].split('.', 1)[0], dirs[1].split('.', 1)[1]]
+    if file0[1] == 'txt':
+        txtfile = file0[0] + '.' + file0[1]
+        xlsxfile = file1[0] + '.' + file1[1]
+    else:
+        txtfile = file1[0] + '.' + file1[1]
+        xlsxfile = file0[0] + '.' + file0[1]
+    filename = remove_columns(txtfile, xlsxfile)
+    return render_template("col_rm/removed.html", filse=filename)
+
+
+@bp.route('/download/')
+def download_file():
+    dirs = os.listdir(path1)
+    finalf = path1 + '/' + dirs[0]
+    print(path1, '/', finalf)
+    path2 = "static/col_rm_final"
+    finalf = dirs[0]
+    try:
+        send_from_directory(path2, filename=finalf, as_attachment=True)
+        return render_template("col_rm/removed.html")
+    except FileNotFoundError:
+        return render_template("col_rm/removed.html", filse=finalf)
+    return render_template("col_rm/removed.html")
 
 
 def get_all_good_headers(txtfile):
@@ -60,7 +113,8 @@ def get_all_good_headers(txtfile):
 
 
 def remove_columns(txtfile, xlsxfile):
-    excel = xlsxfile
+    excel = path + '/' + xlsxfile
+    txtfile = path + '/' + txtfile
     good = get_all_good_headers(txtfile)
     wbf = oxl.load_workbook(excel)
     sh0 = wbf[wbf.sheetnames[0]]
@@ -85,4 +139,7 @@ def remove_columns(txtfile, xlsxfile):
             number_of_columns -= 1
     finalname = xlsxfile.rsplit(".", 1)
     finale = finalname[0]+"_final.xlsx"
-    wbf.save(finale)
+    wbf.save(path1 + '/' + finale)
+    os.remove(txtfile)
+    os.remove(excel)
+    return finale
